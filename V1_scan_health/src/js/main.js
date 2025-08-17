0d40cb7263d15c5849e9e1a392fa1589aacc5db0
Ahmed Mohamed <69980108+12ahmed2@users.noreply.github.com>
2025-08-17 07:53:36 +0300

    Add files via upload

---
+++ V1_scan_health/src/js/main.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', function() {
+    const myDiv = document.getElementById('loading_page');
+    if (myDiv) { // Check if the element exists
+        myDiv.style.display = 'none'; // Or 'flex', 'grid', etc., depending on desired layout
+    }
+});
