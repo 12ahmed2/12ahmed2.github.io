b5fb38283abe401d28b6f83389d8be29a876463b
Ahmed Mohamed <69980108+12ahmed2@users.noreply.github.com>
2025-08-17 07:56:41 +0300

    Add files via upload

---
+++ src/js/scanner.js
@@ -0,0 +1,14 @@
+function onScanSuccess(decodedText, decodedResult) {
+    // Handle the scanned code
+    document.getElementById('result').innerText = decodedText;
+    // You can redirect or send data to backend here
+    // Example: window.location.href = decodedText;
+  }
+
+function onScanFailure(error) {
+    alert(`Error scanning QR code: ${error}`);
+  }
+
+let html5QrcodeScanner = new Html5QrcodeScanner(
+"qr-reader", { fps: 10, qrbox: 250 });
+html5QrcodeScanner.render(onScanSuccess, onScanFailure);
