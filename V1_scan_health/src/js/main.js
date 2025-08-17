c00634a66ef39deebc913ad825432e57d53888b5
Ahmed Mohamed <69980108+12ahmed2@users.noreply.github.com>
2025-08-17 07:56:10 +0300

    Delete V1_scan_health/src directory

--- V1_scan_health/src/js/main.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('load', function() {
-    const myDiv = document.getElementById('loading_page');
-    if (myDiv) { // Check if the element exists
-        myDiv.style.display = 'none'; // Or 'flex', 'grid', etc., depending on desired layout
-    }
-});
